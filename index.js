65d82bdc28d1d9924e7ec69edcfde4247e248cd4
Matarol <91778333+Matarol@users.noreply.github.com>
2026-03-16 12:34:19 +0100

    Second commit
    Initial collision detection almost in place

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ class Player {
         this.velocity = velocity
         this.radius = 15
     }
+
     draw() {
         c.beginPath()
         c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
@@ -33,15 +34,14 @@ class Player {
         c.fill()
         c.closePath()
     }
+
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
 }
 
-const map = [
-    ['-', '-', '-', '-', '-', '-'],
-    ['-', ' ', ' ', ' ', ' ', '-'],
-    ['-', ' ', '-', '-', ' ', '-'],
-    ['-', ' ', ' ', ' ', ' ', '-'],
-    ['-', '-', '-', '-', '-', '-']
-]
 
 const boundaries = []
 const player = new Player({
@@ -54,6 +54,31 @@ const player = new Player({
         y: 0
     }
 })
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
+
+let lastKey = ''
+
+const map = [
+    ['-', '-', '-', '-', '-', '-'],
+    ['-', ' ', ' ', ' ', ' ', '-'],
+    ['-', ' ', '-', '-', ' ', '-'],
+    ['-', ' ', ' ', ' ', ' ', '-'],
+    ['-', '-', '-', '-', '-', '-']
+]
 
 map.forEach((row, i) => {
     row.forEach((symbol, j) => {
@@ -69,8 +94,61 @@ map.forEach((row, i) => {
     })
 })
 
-boundaries.forEach((boundary) => {
-    boundary.draw()
+function animate() {
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, innerWidth, innerHeight)
+    boundaries.forEach((boundary) => {
+        boundary.draw()
+
+        if (player.position.y <= boundary.position.y + boundary.height) {
+            console.log("Det här borde inte skrivas ut för det finns ju ingen boundary med lägre y-position?")
+        }
+     
+    })
+
+    player.update()
+    player.velocity.y = 0
+    player.velocity.x = 0
+
+    if (keys.w.pressed && lastKey === 'w') {
+        player.velocity.y = -5
+    } else if (keys.a.pressed && lastKey === 'a') {
+        player.velocity.x = -5
+    } else if (keys.s.pressed && lastKey === 's') {
+        player.velocity.y = 5
+    } else if (keys.d.pressed && lastKey === 'd') {
+        player.velocity.x = 5
+    }
+}
+
+animate()
+
+addEventListener('keydown', ({key}) => {
+    switch (key) {
+        case 'w': keys.w.pressed = true
+        lastKey = 'w'
+        break
+        case 'a': keys.a.pressed = true
+        lastKey = 'a'
+        break
+        case 's': keys.s.pressed = true
+        lastKey = 's'
+        break
+        case 'd': keys.d.pressed = true
+        lastKey = 'd'
+        break
+    }
 })
 
-player.draw()
+addEventListener('keyup', ({key}) => {
+    switch (key) {
+        case 'w': keys.w.pressed = false
+        break
+        case 'a': keys.a.pressed = false
+        break
+        case 's': keys.s.pressed = false
+        break
+        case 'd': keys.d.pressed = false
+        break
+    }
+})
