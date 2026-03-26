65c50bda4ca624b1079a11076a56f2f10cf21c91
Matarol <91778333+Matarol@users.noreply.github.com>
2026-03-26 10:14:21 +0100

    Update villainController.js
    Villain is now trying to avoid obstacles.

--- src/villainController.js
+++ src/villainController.js
@@ -9,7 +9,7 @@ export function updateVillain(villain, player, boundaries) {
     // Grundriktning mot spelaren
     const dx = player.position.x - villain.position.x
     const dy = player.position.y - villain.position.y
-    const targetAngle = Math.atan2(dy, dx)
+    let targetAngle = Math.atan2(dy, dx)
 
     //Se hinder framför sig
     const lookAheadDistance = 40
@@ -24,27 +24,28 @@ export function updateVillain(villain, player, boundaries) {
                 y: villain.position.y + Math.sin(checkAngle) * lookAheadDistance
             },
             radius: villain.radius,
-            velocity: { x: 0, y: 0}
+            velocity: { x: 0, y: 0 }
         }
+        for (const boundary of boundaries) {
+            if (boundary.isPortal) continue
+
+            if (circleCollidesWithRectangle({ circle: checkPoint, rectangle: boundary })) {
+                targetAngle -= offset * 2
+                obstruction = true
+                break
+            }
+        }
+
     }
 
-    for (const boundary of boundaries) {
-        if (boundary.isPortal) continue
-
-        if (circleCollidesWithRectangle({ circle: checkPoint, rectangle: boundary }))
-            targetAngle -= offset * 2
-            obstruction = true
-            break
-        
-    }
-
+    //Applicera den eventuellt justerade vinkeln
     const nextVelocity = {
-        x: Math.cos(angle) * speed,
-        y: Math.sin(angle) * speed
+        x: Math.cos(targetAngle) * speed,
+        y: Math.sin(targetAngle) * speed
     }
 
-    let blocked = false
-
+    //Slutgiltig krockcheck
+    let finalBlocked = false
     for (const boundary of boundaries) {
         if (boundary.isPortal) continue
 
@@ -52,19 +53,20 @@ export function updateVillain(villain, player, boundaries) {
             circle: { ...villain, velocity: nextVelocity },
             rectangle: boundary
         })) {
-            blocked = true
+            finalBlocked = true
             break
         }
     }
 
-    if (!blocked) {
+    if (!finalBlocked) {
         villain.velocity.x = nextVelocity.x
         villain.velocity.y = nextVelocity.y
-        villain.rotation = angle
+        villain.rotation = targetAngle
     } else {
-        villain.velocity.x = 0
-        villain.velocity.y = 0
+        villain.velocity.x *= 0.5
+        villain.velocity.y *= 0.5
     }
 
     villain.update()
+
 }
